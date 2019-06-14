3e5e3e94baeb3b5b86bee7a77bf1b9ded78d2838
swomics <44398000+swomics@users.noreply.github.com>
2019-06-14 14:32:30 +0100

    Create Get_fastqs.sh

---
+++ CAT-annotation/Get_fastqs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+##loop through a bunch of species directories and download fastqs from curated SRA tables
+
+for dir in $(ls); do
+
+cd $dir
+
+for run in $(awk 'NR==1 { for (i=1; i<=NF; i++) { f[$i] = i}}{ print $(f["Run"]) }' *_SRA_table.txt |grep -v "Run"); do
+
+echo ~/bin/sra_current/sratoolkit.2.8.2-1-ubuntu64/bin/fastq-dump --gzip --skip-technical  --readids --dumpbase --split-files --clip $run &
+
+done
+
+cd ..
+
+done
