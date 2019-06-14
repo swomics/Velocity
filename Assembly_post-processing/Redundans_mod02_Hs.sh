b1290ef15d97139e7d17b110e8043f7ea363ae10
swomics <44398000+swomics@users.noreply.github.com>
2019-06-14 16:36:31 +0100

    Create Redundans_mod02_Hs.sh

---
+++ Assembly_post-processing/Redundans_mod02_Hs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+export PATH=~/bin/last-946/bin:$PATH
+export PYTHONPATH=~/bin/redundans/pyScaf:$PYTHONPATH
+export PYTHONPATH=~/bin/redundans/bin:$PYTHONPATH
+export PATH=~/bin/bwa:$PATH
+
+#Hipparchia semele
+
+Species_name="Hipparchia_semele"
+
+mkdir ./$Species_name
+
+cd $Species_name
+
+##These folders need to be tailored according to species
+array_1=$(ls ../../Reseq_trimming_mod02/HS-01-2016/*_paired.fastq | tr '\n' ' ')
+
+
+
+~/bin/redundans/bin/redundans.py -f ../../Platanus2_batch2/${Species_name}_consensusScaffold.fa --nogapclosing -b -t 40 -o ${Species_name}_red -i ../../NGX_CGR_trimmed_fastqs/Trimmed/${Species_name}/*_R1_001.fastq ../../NGX_CGR_trimmed_fastqs/Trimmed/${Species_name}/*_R2_001.fastq ${array_1}
+
+python ~/bin/MESPA/mespa_17aug15.py -a ${Species_name}_red/scaffolds.fa -p ~/Velocity/Hmel2.5_LG/Hmel_2.5_Prot_corrected.fa -c 4 -s ~/bin/MESPA/mespa.cfg
