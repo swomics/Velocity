cd0eb250ce111a9f2ea600371497cea0f31f93ef
swomics <44398000+swomics@users.noreply.github.com>
2019-06-14 15:51:12 +0100

    Update Get_fastqs.sh

--- CAT-annotation/Get_fastqs.sh
+++ CAT-annotation/Get_fastqs.sh
@@ -9,7 +9,7 @@ cd $dir
 
 for run in $(awk 'NR==1 { for (i=1; i<=NF; i++) { f[$i] = i}}{ print $(f["Run"]) }' *_SRA_table.txt |grep -v "Run"); do
 
-echo ~/bin/sra_current/sratoolkit.2.8.2-1-ubuntu64/bin/fastq-dump --gzip --skip-technical  --readids --dumpbase --split-files --clip $run &
+echo ~/bin/sra_current/sratoolkit.2.8.2-1-ubuntu64/bin/fastq-dump --gzip --skip-technical  --readids --dumpbase --split-files --clip $run
 
 done
 
